3d7003b53ae91657b51b9e5cc45a980b351a8a91
frank <suxiang663215@gmail.com>
2019-06-16 17:04:40 +1000

    new change

---
+++ src/HandApp.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>hello worlddddd
+      </div>
+
+    )
+  }
+}
+
+
+
+export default App;
